3f857b583c67c3e40a6a7dfa66288fe1bb80b586
azfarmasood <azfarmasood22@gmail.com>
2026-01-31 23:05:21 +0500

    updated

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://79e66ea0060e.ngrok-free.app/api",
+  baseURL: "https://2610f73e238a.ngrok-free.app/api",
   headers: {
     "Content-Type": "application/json",
     "ngrok-skip-browser-warning": "true",
